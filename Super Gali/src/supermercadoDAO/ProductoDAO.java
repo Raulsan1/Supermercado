5d4fdada5e31eaafa3c4b463eb8a6cc51849406f
alumno <alumno@PC09>
2022-05-26 13:53:53 +0200

    1

--- Super Gali/src/supermercadoDAO/ProductoDAO.java
+++ Super Gali/src/supermercadoDAO/ProductoDAO.java
@@ -1,12 +1,41 @@
 package supermercadoDAO;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import juego.Conectar;
 import supermercadoModelo.ProductoDTO;
 
 public class ProductoDAO {
 
-	/*
-	public buscarProducto (ProductoDTO dto) {
+
+	public void buscarProducto (ProductoDTO dto) {
 		
+		try {
+			
+			Conectar con = new Conectar ();
+			Statement consulta = con.getConnect().createStatement();
+			
+			if (!dto.getCodProducto().equals("")) {
+				
+				ResultSet resultado = consulta.executeQuery("SELECT * FROM Personaje WHERE nombre = '"+dto.getCodProducto()+"';");
+				
+				if (resultado.next()==true) {
+					System.out.println("El personaje se encuentra en la base de datos.");
+					
+				}else {
+					System.out.println("El personaje no se encuentra en la base de datos.");
+				}
+				
+				resultado.close();
+			}
+			consulta.close();
+			con.cerrarConexion(con.getConnect());
+	
+			
+		} catch (Exception e) {
+			System.out.println("Error en la consulta: "+e.getLocalizedMessage());
+		}
 	}
-	*/
+	
 }
